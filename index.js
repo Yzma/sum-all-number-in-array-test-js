28e713d1bc54cb3179e866b3dc59869ed04ecc65
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-13 12:59:57 -0500

    Implemnent sunItems

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+const sumItems = function(array) {
+  if (array.length === 0) {
+    return 0
+  }
+
+  let value = array.pop()
+
+  if (Array.isArray(value)) {
+    value = sumItems(value)
+  }
+
+  return value + sumItems(array)
 }
 
-module.exports = sumItems;
+module.exports = sumItems
